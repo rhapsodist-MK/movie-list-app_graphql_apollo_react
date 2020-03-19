621e50044622ab7e6903c617285c6762aa82776d
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-19 19:16:14 +0900

    add styling && use optional chaining javascript

--- web/src/routes/Home.js
+++ web/src/routes/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
+import styled from 'styled-components'
+
+import Movie from '../components/Movie'
 
 const GET_MOVIES = gql`
   {
@@ -11,13 +14,70 @@ const GET_MOVIES = gql`
   }
 `
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES)
   
-  if (loading) {
-    return 'loading...'
-  }
-  if (data && data.movies) {
-    return data.movies.map(m => <h1>{m.id}</h1>)
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Movie List</Title>
+        <Subtitle>GraphQl</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <Movies>
+        {/* optional chaining javascript */}
+        {data?.movies?.map(m => (
+          <Movie
+            key={m.id}
+            id={m.id}
+            bg={m.medium_cover_image}
+          />
+        ))}
+      </Movies>
+    </Container>
+  )
 }
